b186ea888cebffc1e5fb468c46ec907aaf42f08a
whayes <whayes@cvg-000211M-090.ga.afginc.com>
2019-03-04 21:28:09 -0500

    Wanda - creating regression test folder

---
+++ Test Suites/TS_Regression_ForTeamXYZ/TS_RegressionSuite_QuickDemo.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Run quick regression during demo</description>
+   <name>TS_RegressionSuite_QuickDemo</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>2238f948-f879-4a5d-88f6-52d86420b66c</testSuiteGuid>
+   <testCaseLink>
+      <guid>439ff712-2fc7-4efe-9f7d-3e4e1c10d859</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/WEBUI_Demo/_DemoEval/AUT_Appointments/TC_Login</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
